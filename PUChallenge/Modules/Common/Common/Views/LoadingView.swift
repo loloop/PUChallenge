963d5315f44aea477e7dcc8a32d15dcc1b210c6a
Mauricio Cardozo <loloop@live.com>
2019-11-13 15:35:07 -0300

    Adds LoadingView

---
+++ PUChallenge/Modules/Common/Common/Views/LoadingView.swift
@@ -0,0 +1,49 @@
+//
+//  LoadingView.swift
+//  Common
+//
+//  Created by Mauricio Cardozo on 11/13/19.
+//  Copyright © 2019 Mauricio Cardozo. All rights reserved.
+//
+
+import UIKit
+
+public final class LoadingView: UIView {
+
+    private lazy var loadingConstraints: [NSLayoutConstraint] = [
+        activityView.centerXAnchor.constraint(equalTo: centerXAnchor),
+        activityView.centerYAnchor.constraint(equalTo: centerYAnchor)
+    ]
+
+    private lazy var activityView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    public init() {
+        super.init(frame: .zero)
+        setupViews()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+extension LoadingView: CodableView {
+    public func setupViewHierarchy() {
+        addSubview(activityView)
+    }
+
+    public func setupConstraints() {
+        addConstraints(loadingConstraints)
+    }
+
+    public func configureViews() {
+        activityView.startAnimating()
+        backgroundColor = .white
+    }
+}
